88eaaefb9e24687d1bee6afb9bf650590647e11f
sanjaydeveloper4 <developer4@iorange.in>
2026-07-07 18:29:17 +0530

    Use updated COPAR logo asset

--- resources/views/cirkla/includes/head.blade.php
+++ resources/views/cirkla/includes/head.blade.php
@@ -3,8 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ $metaDescription ?? 'COPAR sustainable tray packaging made from circular materials.' }}">
     <title>{!! $pageTitle ?? 'COPAR - Sustainable Tray Packaging' !!}</title>
-    <link rel="icon" href="/images/copar/logo.svg" type="image/svg+xml">
-    <link rel="shortcut icon" href="/images/copar/logo.svg">
+    <link rel="icon" href="/images/logo.svg" type="image/svg+xml">
+    <link rel="shortcut icon" href="/images/logo.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
     <link href="/css2?family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,400;0,9..144,500;0,9..144,600;1,9..144,300;1,9..144,400&family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">

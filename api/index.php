09e5bdad82adec7415ec19c5086b5803a7fab5ee
sanjaydeveloper4 <developer4@iorange.in>
2026-07-07 15:16:35 +0530

    Prepare Vercel runtime view cache

--- api/index.php
+++ api/index.php
@@ -5,6 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$compiledViewPath = getenv('VIEW_COMPILED_PATH') ?: sys_get_temp_dir().'/views';
+
+if (! is_dir($compiledViewPath)) {
+    mkdir($compiledViewPath, 0777, true);
+}
+
+putenv("VIEW_COMPILED_PATH={$compiledViewPath}");
+$_ENV['VIEW_COMPILED_PATH'] = $compiledViewPath;
+$_SERVER['VIEW_COMPILED_PATH'] = $compiledViewPath;
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
